7036aad71a4cd0bb9e1f60ed66c4c7d99e85e78a
Eduardo Martin Lorenzo <eduardomartinlorenzo@Mac-Studio.home>
2022-09-01 17:27:17 +0200

    Se añade la BottomAppBar

--- app/src/main/java/com/emlcoding/marvelcompose/ui/common/CharacterDetailScaffold.kt
+++ app/src/main/java/com/emlcoding/marvelcompose/ui/common/CharacterDetailScaffold.kt
@@ -2,10 +2,15 @@ package com.emlcoding.marvelcompose.ui.common
 
 import android.content.Context
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material.icons.filled.Share
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.core.app.ShareCompat
@@ -36,6 +41,19 @@ fun CharacterDetailScaffold(
         },
         // Por defecto el FloatingActionButton se pone a la derecha, pero con la siguiente linea se podria poner en el centro
         floatingActionButtonPosition = FabPosition.Center,
+        isFloatingActionButtonDocked = true, // Esto, junto al "cutoutShape" de la bottomBar permite hacer la integracion de los dos componentes visualmente. Esta propiedad por defecto esta a false
+        bottomBar = {
+                  BottomAppBar(
+                      cutoutShape = CircleShape // Aqui hay que elegir la misma forma que tenga el FloatingActionButton para poder hacer el efecto visual. Esta propiedad en concreto es la que hace el efecto de recorte
+                  ) {
+
+                      AppBarIcon(imageVector = Icons.Default.Menu, onClick = { /*TODO*/ })
+                      
+                      Spacer(modifier = Modifier.weight(1f))
+
+                      AppBarIcon(imageVector = Icons.Default.Favorite, onClick = { /*TODO*/ })
+                  }
+        },
         content = content
     )
 }
