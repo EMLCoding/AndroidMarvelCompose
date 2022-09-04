b54f196f93bdb95733b699f4b636a35b2fb0afd9
Eduardo Martin Lorenzo <eduardomartinlorenzo@Mac-Studio.home>
2022-09-04 18:15:24 +0200

    Establecer estilos a todos los elementos dentro de un bloque

--- app/src/main/java/com/emlcoding/marvelcompose/navigation/DrawerContent.kt
+++ app/src/main/java/com/emlcoding/marvelcompose/navigation/DrawerContent.kt
@@ -4,11 +4,9 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.ContentAlpha
-import androidx.compose.material.Icon
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Brush
@@ -42,30 +40,35 @@ fun DrawerContent(
         val selected = selectedIndex == index
         val colors = MaterialTheme.colors
 
-        Row(
-            modifier = Modifier
-                .clickable { onOptionClick(navItem) }
-                .fillMaxWidth()
-                .padding(8.dp, 4.dp)
-                .clip(RoundedCornerShape(4.dp))
-                .background(color = if(selected) colors.primary.copy(alpha = 0.12f) else colors.surface)
-                .padding(12.dp)
+        val localContentColor = if(selected) colors.primary else colors.onBackground
+
+        // Permite establecer estilos a todos los elementos que estén dentro
+        CompositionLocalProvider(
+            LocalTextStyle provides MaterialTheme.typography.body1.copy(fontWeight = FontWeight.Bold),
+            LocalContentColor provides  localContentColor
         ) {
-            Icon(
-                imageVector = navItem.icon,
-                tint = if(selected) colors.primary else colors.onSurface.copy(
-                    alpha = ContentAlpha.medium
-                ),
-                contentDescription = navItem.name
-            )
+            Row(
+                modifier = Modifier
+                    .clickable { onOptionClick(navItem) }
+                    .fillMaxWidth()
+                    .padding(8.dp, 4.dp)
+                    .clip(RoundedCornerShape(4.dp))
+                    .background(color = if (selected) LocalContentColor.current.copy(alpha = 0.12f) else colors.surface)
+                    .padding(12.dp)
+            ) {
+                CompositionLocalProvider(LocalContentAlpha provides ContentAlpha.medium) {
+                    Icon(
+                        imageVector = navItem.icon,
+                        contentDescription = navItem.name
+                    )
+                }
 
-            Spacer(modifier = Modifier.width(24.dp))
+                Spacer(modifier = Modifier.width(24.dp))
 
-            Text(
-                text = stringResource(id = navItem.title),
-                color = if(selected) colors.primary else colors.onSurface,
-                style = MaterialTheme.typography.body1.copy(fontWeight = FontWeight.Bold)
-            )
+                Text(text = stringResource(id = navItem.title))
+            }
         }
+
+
     }
 }
