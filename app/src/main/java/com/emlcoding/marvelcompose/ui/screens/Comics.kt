8ff5f7a35cbd478868d10c3f7189f0daf38411d4
Eduardo Martin Lorenzo <eduardomartinlorenzo@Mac-Studio.home>
2022-09-04 10:36:03 +0200

    Scroll en las pestañas de la pantalla de Comics

--- app/src/main/java/com/emlcoding/marvelcompose/ui/screens/Comics.kt
+++ app/src/main/java/com/emlcoding/marvelcompose/ui/screens/Comics.kt
@@ -1,11 +1,13 @@
 package com.emlcoding.marvelcompose.ui.screens
 
+import androidx.annotation.StringRes
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import com.emlcoding.marvelcompose.R
 import com.emlcoding.marvelcompose.models.Comic
 import com.emlcoding.marvelcompose.repositories.ComicsRepository
@@ -14,7 +16,9 @@ import com.emlcoding.marvelcompose.ui.common.MarvelItemsList
 import com.emlcoding.marvelcompose.ui.common.MarvelItemsListScreen
 import com.google.accompanist.pager.ExperimentalPagerApi
 import com.google.accompanist.pager.HorizontalPager
+import com.google.accompanist.pager.PagerState
 import com.google.accompanist.pager.rememberPagerState
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.launch
 
 @OptIn(ExperimentalPagerApi::class)
@@ -26,19 +30,12 @@ fun ComicsScreen(onClick: (Comic) -> Unit) {
         comicsState = ComicsRepository.get()
     }
 
-    val formats = Comic.Format.values().take(3)
+    val formats = Comic.Format.values().toList()
     val pagerState = rememberPagerState()
-    val scope = rememberCoroutineScope()
+
 
     Column {
-        TabRow(selectedTabIndex = pagerState.currentPage) {
-            formats.forEach {
-                Tab(
-                    selected = it.ordinal == pagerState.currentPage,
-                    onClick = { scope.launch { pagerState.animateScrollToPage(it.ordinal) } },
-                    text = { Text(text = it.name) })
-            }
-        }
+        ComicFormatsTabRow(pagerState, formats)
 
         HorizontalPager(
             count = formats.size,
@@ -48,6 +45,36 @@ fun ComicsScreen(onClick: (Comic) -> Unit) {
         }
     }
 
+}
+
+@OptIn(ExperimentalPagerApi::class)
+@Composable
+private fun ComicFormatsTabRow(
+    pagerState: PagerState,
+    formats: List<Comic.Format>
+) {
+    val scope = rememberCoroutineScope()
+    // Por defecto el ScrollableTabRow deja un hueco a la izquierda por si se quiere insertar un icono. Para quitar este hueco hay que usar el edgePadding
+    ScrollableTabRow(selectedTabIndex = pagerState.currentPage, edgePadding = 0.dp) {
+        formats.forEach {
+            Tab(
+                selected = it.ordinal == pagerState.currentPage,
+                onClick = { scope.launch { pagerState.animateScrollToPage(it.ordinal) } },
+                text = { Text(text = stringResource(id = it.toStringRes())) })
+        }
+    }
+}
+
+@StringRes
+private fun Comic.Format.toStringRes(): Int = when(this) {
+    Comic.Format.COMIC -> R.string.comic
+    Comic.Format.MAGAZINE -> R.string.magazine
+    Comic.Format.TRADE_PAPERBACK -> R.string.trade_paperback
+    Comic.Format.HARDCOVER -> R.string.hardcover
+    Comic.Format.DIGEST -> R.string.digest
+    Comic.Format.GRAPHIC_NOVEL -> R.string.graphic_novel
+    Comic.Format.DIGITAL_COMIC -> R.string.digital_comic
+    Comic.Format.INFINITE_COMIC -> R.string.infinite_comic
 }
 
 @Composable
